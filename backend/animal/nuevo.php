a778f3070e12ff7da1cf6ec4ea54e633a7a43e21
Axel Morales Torres <axelzkai.123@gmail.com>
2022-03-17 16:24:55 -0600

    Added missing foreign key

--- backend/animal/nuevo.php
+++ backend/animal/nuevo.php
@@ -3,8 +3,8 @@ require_once('../includes/db.inc.php');
 require_once('../includes/util.inc.php');
 $data = escapeArray($_POST, $db_con);
 $dir_pic = uploadFile($_POST['foto']);
-$stmt = $db_con->prepare('INSERT into animal(nombre, edad, foto, idEspecie) VALUES(?,?,?,?)');
-$stmt->bind_param('sisi', $data['nombre'], $data['edad'], $dir_pic, $data['especie']);
+$stmt = $db_con->prepare('INSERT into animal(nombre, edad, foto, idEspecie, idContacto) VALUES(?,?,?,?,?)');
+$stmt->bind_param('sisii', $data['nombre'], $data['edad'], $dir_pic, $data['idEspecie'], $data['idContacto']);
 $stmt->execute();
 header('Content-Type: application/json');
 if($stmt->affected_rows == 1)
