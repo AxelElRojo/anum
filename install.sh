b813c0591d87b82a9403d805600984ba8fb72492
Axel Morales Torres <axelzkai.123@gmail.com>
2022-03-17 17:25:18 -0600

    Added install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+read -rp "Web server root: " root
+read -rp "Web domain: " domain
+cp -r ./backend "$root/api.$domain"
+cp -r ./frontend "$root/sys.$domain"
+
+mariadb -e "CREATE DATABASE anum;"
+mariadb -e "CREATE USER anum_admin@localhost IDENTIFIED BY 'passwd';"
+mariadb -e "GRANT ALL PRIVILEGES ON anum.* TO 'anum_admin'@'localhost';"
+mariadb -e "FLUSH PRIVILEGES;"
+mysqldump -u anum -ppasswd anum > database/anum.sql
+
+echo "Done, make sure you create a website config for:"
+printf "\t - api.%s\n" "$domain"
+printf "\t - sys.%s\n" "$domain"
