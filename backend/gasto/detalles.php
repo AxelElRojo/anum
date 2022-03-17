f97dda0933c42c0516de170d94a811bdeea7682f
Axel Morales Torres <axelzkai.123@gmail.com>
2022-03-17 16:21:02 -0600

    Added "gasto" API

---
+++ backend/gasto/detalles.php
@@ -0,0 +1,13 @@
+<?php
+require_once('../includes/db.inc.php');
+require_once('../includes/util.inc.php');
+$data = escapeArray($_POST, $db_con);
+$stmt = $db_con->prepare('SELECT * FROM gasto WHERE id=?');
+$stmt->bind_param('i', $data['id']);
+$stmt->execute();
+$res = $stmt->get_result();
+$data = [];
+while($row = $res->fetch_assoc())
+	array_push($data, $row);
+header('Content-Type: application/json');
+echo json_encode($data);
---
+++ backend/gasto/listar.php
@@ -0,0 +1,11 @@
+<?php
+require_once('../includes/db.inc.php');
+require_once('../includes/util.inc.php');
+$stmt = $db_con->prepare('SELECT id, concepto, fecha, cantidad FROM gasto WHERE eliminado=0');
+$stmt->execute();
+$res = $stmt->get_result();
+$data = [];
+while($row = $res->fetch_assoc())
+	array_push($data, $row);
+header('Content-Type: application/json');
+echo json_encode($data);
