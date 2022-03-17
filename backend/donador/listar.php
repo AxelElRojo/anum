0159cdc06eca32b576a5d306969397e983a47f48
Axel Morales Torres <axelzkai.123@gmail.com>
2022-03-17 16:41:30 -0600

    Added "donador" API

---
+++ backend/donador/listar.php
@@ -0,0 +1,11 @@
+<?php
+require_once('../includes/db.inc.php');
+require_once('../includes/util.inc.php');
+$stmt = $db_con->prepare('SELECT id, nombre, rfc FROM donador WHERE eliminado=0');
+$stmt->execute();
+$data = [];
+$res = $stmt->get_result();
+while($row = $res->fetch_assoc())
+	array_push($data, $row);
+header('Content-Type: application/json');
+echo json_encode($data);
