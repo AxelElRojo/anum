4ac04e9fb84ed3f2f6ee0dba8c1d751f58c02463
Axel Morales Torres <axelzkai.123@gmail.com>
2022-03-17 16:48:17 -0600

    Added "donaciones" API

---
+++ backend/donacion/nuevo.php
@@ -0,0 +1,9 @@
+<?php
+require_once('../includes/db.inc.php');
+require_once('../includes/util.inc.php');
+$data = escapeArray($_POST, $db_con);
+$stmt = $db_con->prepare('INSERT INTO donaciones(fecha, cantidad, idDonador) VALUES(?,?,?)');
+$stmt->bind_param('sdi', $data['fecha'], $data['cantidad'], $data['idDonador']);
+$stmt->execute();
+header('Content-Type: application/json');
+echo json_encode(array('exito' => $stmt->affected_rows == 1));
