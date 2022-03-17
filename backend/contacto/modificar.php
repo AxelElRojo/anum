1453008ca7d7fe78e181defc8ebabc0f2f667532
Axel Morales Torres <axelzkai.123@gmail.com>
2022-03-17 16:33:00 -0600

    Added "contacto" API

---
+++ backend/contacto/modificar.php
@@ -0,0 +1,9 @@
+<?php
+require_once('../includes/db.inc.php');
+require_once('../includes/util.inc.php');
+$data = escapeArray($_POST, $db_con);
+$stmt = $db_con->prepare('UPDATE contacto SET nombre=?, correo=?, telefono=? WHERE id=?');
+$stmt->bind_param('sssi', $data['nombre'], $data['correo'], $data['telefono'], $data['id']);
+$stmt->execute();
+header('Content-Type: application/json');
+echo json_encode(array('exito' => $stmt->affected_rows));
