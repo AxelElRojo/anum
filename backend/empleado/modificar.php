ca3e25a1f0a9ccd44f8dc59def7bd235ff15572e
Axel Morales Torres <axelzkai.123@gmail.com>
2022-03-18 12:09:06 -0600

    Using password_hash

--- backend/empleado/modificar.php
+++ backend/empleado/modificar.php
@@ -3,6 +3,7 @@ require_once('../.includes/db.inc.php');
 require_once('../.includes/util.inc.php');
 $data = escapeArray($_POST, $db_con);
 $data['esAdmin'] = filter_var($data['esAdmin'], FILTER_VALIDATE_BOOLEAN);
+$data['contraseña'] = password_hash($data['contraseña'], PASSWORD_BCRYPT);
 $stmt = $db_con->prepare('UPDATE empleado SET usuario=?, nombre=?, contraseña=?, correo=?, area=?, esAdmin=? WHERE id=?');
 $stmt->bind_param('sssssii', $data['usuario'], $data['nombre'], $data['contraseña'], $data['correo'], $data['area'], $data['esAdmin'], $data['id']);
 $stmt->execute();
--- backend/empleado/nuevo.php
+++ backend/empleado/nuevo.php
@@ -3,6 +3,7 @@ require_once('../.includes/db.inc.php');
 require_once('../.includes/util.inc.php');
 $data = escapeArray($_POST, $db_con);
 $data['esAdmin'] = filter_var($data['esAdmin'], FILTER_VALIDATE_BOOLEAN);
+$data['contraseña'] = password_hash($data['contraseña'], PASSWORD_BCRYPT);
 $stmt = $db_con->prepare('INSERT INTO empleado(usuario, nombre, contraseña, correo, area, esAdmin) VALUES(?,?,?,?,?,?)');
 $stmt->bind_param('sssssi', $data['usuario'], $data['nombre'], $data['contraseña'], $data['correo'], $data['area'], $data['esAdmin']);
 $stmt->execute();
