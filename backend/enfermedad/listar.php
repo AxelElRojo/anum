cea5ff35e153a84a310c4def4a29b60cb960c092
Axel Morales Torres <axelzkai.123@gmail.com>
2022-03-18 10:38:06 -0600

    Fixed list APIs

--- backend/enfermedad/listar.php
+++ backend/enfermedad/listar.php
@@ -2,7 +2,7 @@
 require_once('../.includes/db.inc.php');
 require_once('../.includes/util.inc.php');
 $data = escapeArray($_POST, $db_con);
-$stmt = $db_con->prepare('SELECT nombre, descripcion FROM enfermedad WHERE idAnimal=?');
+$stmt = $db_con->prepare('SELECT id, nombre, descripcion FROM enfermedad WHERE idAnimal=? AND eliminado=0');
 $stmt->prepare('i', $data['idAnimal']);
 $stmt->execute();
 $res = $stmt->get_result();
--- backend/veterinario/listar.php
+++ backend/veterinario/listar.php
@@ -1,7 +1,7 @@
 <?php
 require_once('../.includes/db.inc.php');
 require_once('../.includes/util.inc.php');
-$stmt = $db_con->prepare('SELECT nombre, telefono, direccion FROM veterinario WHERE eliminado=0');
+$stmt = $db_con->prepare('SELECT id, nombre, telefono, direccion FROM veterinario WHERE eliminado=0');
 $stmt->execute();
 $res = $stmt->get_result();
 $data = [];
