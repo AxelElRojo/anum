bb0824bb00987ec4d3b2ca9741ab9435c29e5b67
Axel Morales Torres <axelzkai.123@gmail.com>
2022-03-18 12:22:48 -0600

    Checking if email is already used

--- backend/empleado/modificar.php
+++ backend/empleado/modificar.php
@@ -4,8 +4,12 @@ require_once('../.includes/util.inc.php');
 $data = escapeArray($_POST, $db_con);
 $data['esAdmin'] = filter_var($data['esAdmin'], FILTER_VALIDATE_BOOLEAN);
 $data['contraseña'] = password_hash($data['contraseña'], PASSWORD_BCRYPT);
-$stmt = $db_con->prepare('UPDATE empleado SET usuario=?, nombre=?, contraseña=?, correo=?, area=?, esAdmin=? WHERE id=?');
-$stmt->bind_param('sssssii', $data['usuario'], $data['nombre'], $data['contraseña'], $data['correo'], $data['area'], $data['esAdmin'], $data['id']);
-$stmt->execute();
+if(!isEmailRegistered($data['correo'], $db_con)){
+	$stmt = $db_con->prepare('UPDATE empleado SET usuario=?, nombre=?, contraseña=?, correo=?, area=?, esAdmin=? WHERE id=?');
+	$stmt->bind_param('sssssii', $data['usuario'], $data['nombre'], $data['contraseña'], $data['correo'], $data['area'], $data['esAdmin'], $data['id']);
+	$stmt->execute();
+	$success = $stmt->affected_rows == 1;
+}else
+	$success = false;
 header('Content-Type: application/json');
-echo json_encode(array("exito" => $stmt->affected_rows));
+echo json_encode(array('exito' => $success));
--- backend/empleado/nuevo.php
+++ backend/empleado/nuevo.php
@@ -4,8 +4,12 @@ require_once('../.includes/util.inc.php');
 $data = escapeArray($_POST, $db_con);
 $data['esAdmin'] = filter_var($data['esAdmin'], FILTER_VALIDATE_BOOLEAN);
 $data['contraseña'] = password_hash($data['contraseña'], PASSWORD_BCRYPT);
-$stmt = $db_con->prepare('INSERT INTO empleado(usuario, nombre, contraseña, correo, area, esAdmin) VALUES(?,?,?,?,?,?)');
-$stmt->bind_param('sssssi', $data['usuario'], $data['nombre'], $data['contraseña'], $data['correo'], $data['area'], $data['esAdmin']);
-$stmt->execute();
+if(!isEmailRegistered($data['correo'], $db_con)){
+	$stmt = $db_con->prepare('INSERT INTO empleado(usuario, nombre, contraseña, correo, area, esAdmin) VALUES(?,?,?,?,?,?)');
+	$stmt->bind_param('sssssi', $data['usuario'], $data['nombre'], $data['contraseña'], $data['correo'], $data['area'], $data['esAdmin']);
+	$stmt->execute();
+	$success = $stmt->affected_rows == 1;
+}else
+	$success = false;
 header('Content-Type: application/json');
-echo json_encode(array("exito" => $stmt->affected_rows));
+echo json_encode(array('exito' => $success));
