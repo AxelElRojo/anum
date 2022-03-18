c768624dad565472531e4ce99706c042be661a04
Axel Morales Torres <axelzkai.123@gmail.com>
2022-03-18 10:29:11 -0600

    Added "veterinario" API

---
+++ backend/veterinario/listar.php
@@ -0,0 +1,11 @@
+<?php
+require_once('../.includes/db.inc.php');
+require_once('../.includes/util.inc.php');
+$stmt = $db_con->prepare('SELECT nombre, telefono, direccion FROM veterinario WHERE eliminado=0');
+$stmt->execute();
+$res = $stmt->get_result();
+$data = [];
+while($row = $res->fetch_assoc())
+	array_push($data, $row);
+header('Content-Type: application/json');
+echo json_encode($data);
