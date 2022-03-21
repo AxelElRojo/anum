2f5250966cce2397c41ddd5664077828012c24d0
Axel Morales Torres <axelzkai.123@gmail.com>
2022-03-21 15:15:42 -0600

    Fixed method called

--- backend/enfermedad/listar.php
+++ backend/enfermedad/listar.php
@@ -3,7 +3,7 @@ require_once('../.includes/db.inc.php');
 require_once('../.includes/util.inc.php');
 $data = escapeArray($_POST, $db_con);
 $stmt = $db_con->prepare('SELECT id, nombre, descripcion FROM enfermedad WHERE idAnimal=? AND eliminado=0');
-$stmt->prepare('i', $data['idAnimal']);
+$stmt->bind_param('i', $data['idAnimal']);
 $stmt->execute();
 $res = $stmt->get_result();
 $data = [];
