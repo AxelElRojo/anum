ffb9f481c1694e79c47c0119ba7dc9195d127a1d
Axel Morales Torres <axelzkai.123@gmail.com>
2022-03-27 14:02:35 -0600

    No sé qué pasó

--- backend/vacunacion/listar.php
+++ backend/vacunacion/listar.php
@@ -1,15 +1,9 @@
 <?php
 require_once('../.includes/db.inc.php');
 require_once('../.includes/util.inc.php');
-<<<<<<< HEAD
 $data = escapeArray($_POST, $db_con);
 $stmt = $db_con->prepare('SELECT id, tipo, marca FROM vacunacion WHERE eliminado=0 AND idAnimal=?');
 $stmt->bind_param('i', $data['idAnimal']);
-=======
-// $data = escapeArray($_POST, $db_con);
-$stmt = $db_con->prepare('SELECT id, tipo, marca FROM vacunacion WHERE eliminado=0');
-// $stmt->bind_param('i', $data['idAnimal']);
->>>>>>> 4ea6ecd43b03975e96e757489393b30009bea14c
 $stmt->execute();
 $res = $stmt->get_result();
 $data = [];
