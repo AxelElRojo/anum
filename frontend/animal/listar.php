8e03f8b9128c0c475ffc7699c1e2d6344b95bda8
Axel Morales <axel@moralestorres.dev>
2022-04-20 13:12:17 -0500

    Fixed includes

--- frontend/animal/listar.php
+++ frontend/animal/listar.php
@@ -6,9 +6,11 @@
 	<title>Lista de animales</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-	<script type="text/javascript" src="../js/app.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+	<script type="text/javascript" src="../includes/js/app.js"></script>
+	<script type="text/javascript" src="../includes/js/animal.js"></script>
+	<script type="text/javascript" src="../includes/js/especie.js"></script>
 </head>
 <body>
 	<?php
