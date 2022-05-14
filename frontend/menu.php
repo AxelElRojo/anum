7424231f8b4bc12d59d5e094933803b0b276405e
Axel Morales Torres <axelzkai.123@gmail.com>
2022-05-14 14:45:58 -0500

    Agregado gráfico

--- frontend/menu.php
+++ frontend/menu.php
@@ -15,6 +15,7 @@ if(!session_exists()){
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.js"></script>
 	<script src="./includes/js/app.js"></script>
 </head>
 <body>
@@ -24,5 +25,55 @@ if(!session_exists()){
 		else
 			echo file_get_contents('./includes/header.html');
 	?>
+	<canvas id="grafico" height="50" width="150"></canvas>
 </body>
+<script>
+	const ctx = document.getElementById('grafico').getContext('2d');
+	$.ajax({
+		method: "POST",
+		url: "http://localhost/anum/backend/reporte.php",
+		success: (response) => {
+			const myChart = new Chart(ctx, {
+				type: 'bar',
+				data: {
+					labels: ['Donaciones $MXN', 'Gastos $MXN', 'Ahorros $MXN'],
+					datasets: [{
+						data: [response.mensual.donacion.total, response.mensual.gasto.total, response.mensual.total],
+						backgroundColor: [
+							'rgba(86, 240, 86, 0.2)',
+							'rgba(255, 99, 132, 0.2)',
+							'rgba(54, 162, 235, 0.2)',
+						],
+						borderColor: [
+							'rgba(86, 240, 86, 1)',
+							'rgba(255, 99, 132, 1)',
+							'rgba(54, 162, 235, 1)',
+						],
+						borderWidth: 1
+					}]
+				},
+				options: {
+					responsive: true,
+					scales: {
+						y: {
+							beginAtZero: true
+						}
+					},
+					plugins: {
+						title: {
+							display: true,
+							text: 'Donaciones y gastos en el último mes',
+						},
+						legend: {
+							display: false
+						}
+					}
+				}
+			});
+		},
+		error : (request, status, error) => {
+			console.log(request.responseText, status, error);
+		}
+	})
+</script>
 </html>
