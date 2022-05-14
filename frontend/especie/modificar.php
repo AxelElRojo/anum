8c8d0b34600dc0458a1fd5c56ec872668aa0adf8
Axel Morales Torres <axelzkai.123@gmail.com>
2022-05-14 01:15:45 -0500

    Arreglado el orden de includes

--- frontend/especie/modificar.php
+++ frontend/especie/modificar.php
@@ -8,8 +8,8 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-	<script type="text/javascript" src="../includes/js/especie.js"></script>
 	<script type="text/javascript" src="../includes/js/app.js"></script>
+	<script type="text/javascript" src="../includes/js/especie.js"></script>
 </head>
 <body>
 	<?php
