6d36df3258e27c9765fe3e7ff3251bd551dd6a28
Axel Morales Torres <axelzkai.123@gmail.com>
2022-05-16 22:33:56 -0500

    Arreglado coma faltante

--- backend/animal/modificar.php
+++ backend/animal/modificar.php
@@ -2,7 +2,7 @@
 require_once('../.includes/db.inc.php');
 require_once('../.includes/util.inc.php');
 $data = escapeArray($_POST, $db_con);
-$stmt = $db_con->prepare('UPDATE animal SET nombre=?, edad=? idEspecie=?, idContacto=? WHERE id=?');
+$stmt = $db_con->prepare('UPDATE animal SET nombre=?, edad=?, idEspecie=?, idContacto=? WHERE id=?');
 $stmt->bind_param('siiii', $data['nombre'], $data['edad'], $dir_pic, $data['idEspecie'], $data['idContacto'], $data['id']);
 $stmt->execute();
 header('Content-Type: application/json');
