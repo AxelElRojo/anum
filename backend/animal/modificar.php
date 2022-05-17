5473e3004a7ed036c92433ebc1704584b12389ee
Axel Morales Torres <axelzkai.123@gmail.com>
2022-05-16 22:39:52 -0500

    Eliminado parámetro de sobra

--- backend/animal/modificar.php
+++ backend/animal/modificar.php
@@ -3,7 +3,7 @@ require_once('../.includes/db.inc.php');
 require_once('../.includes/util.inc.php');
 $data = escapeArray($_POST, $db_con);
 $stmt = $db_con->prepare('UPDATE animal SET nombre=?, edad=?, idEspecie=?, idContacto=? WHERE id=?');
-$stmt->bind_param('siiii', $data['nombre'], $data['edad'], $dir_pic, $data['idEspecie'], $data['idContacto'], $data['id']);
+$stmt->bind_param('siiii', $data['nombre'], $data['edad'], $data['idEspecie'], $data['idContacto'], $data['id']);
 $stmt->execute();
 header('Content-Type: application/json');
 echo json_encode(array("exito" => $stmt->affected_rows == 1));
